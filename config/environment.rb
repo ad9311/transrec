f41ffd1fc20aea222a28c5bdc97e218e8dc29766
Ángel Díaz <diazangel9311@gmail.com>
2022-05-06 22:02:24 -0500

    Fix smtp errors

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  password: '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', # This is the secret sendgrid API key which was issued during API key creation
-  domain: 'transrec.herokuapp.com',
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
