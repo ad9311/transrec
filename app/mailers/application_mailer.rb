000128b1ecdfeec1c96355befcf6688589b5c547
Ángel Díaz <diazangel9311@gmail.com>
2022-05-20 20:12:13 -0500

    Remove sendgrid config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'diazangel9311@gmail.com'
+  default from: 'from@example.com'
   layout 'mailer'
 end
--- app/mailers/user_notifier_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserNotifierMailer < ApplicationMailer
-  default from: 'diazangel9311@gmail.com'
-
-  # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_signup_email(user)
-    @user = user
-    mail( to: @user.email, subject: '¡Hola!' )
-  end
-end
